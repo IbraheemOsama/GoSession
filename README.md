146792442cd7641ca943169e371560e740c0b9cd
Ibraheem Osama <ibraheem.osama@gmail.com>
2018-08-28 11:41:30 +1000

    Adding todos
    Adding todos

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 2) Set GOPATH user variable in your OS. (Normally you've a workspace folder that setted in the GOPATH env variable and this folder has a src folder that contains the GoSession repo folder, ex workspace/src/GoSession)
 3) Install packages `go get github.com/BurntSushi/toml github.com/gorilla/mux`
 4) Install VS Code or
-5) At this directory type `go run app.go`
+5) Fix the TODOs
+6) At this directory type `go run app.go`
 
 to test creating new movie use curl
 `curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET http://localhost:3567/movies?page=3`
--- services/movies_service.go
+++ services/movies_service.go
@@ -3,13 +3,7 @@ package services
 import (
 	"GoSession/helpers"
 	"GoSession/models"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"os"
 	"strconv"
-
-	"github.com/gorilla/mux"
 
 	"net/http"
 )
@@ -18,10 +12,9 @@ type MoviesService struct {
 	ResponseHelper *helpers.ResponseHelper
 }
 
-func (service MoviesService) InitAndRegisterService(router *mux.Router) {
-	service.ResponseHelper = &helpers.ResponseHelper{}
-	router.HandleFunc("/movies", service.AllMoviesEndPoint).Methods("GET")
-}
+// TODO to implement interface Register_router_service and include this code in the function
+//service.ResponseHelper = &helpers.ResponseHelper{}
+//router.HandleFunc("/movies", service.AllMoviesEndPoint).Methods("GET")
 
 // GET list of movies
 func (service MoviesService) AllMoviesEndPoint(w http.ResponseWriter, r *http.Request) {
@@ -32,35 +25,17 @@ func (service MoviesService) AllMoviesEndPoint(w http.ResponseWriter, r *http.Re
 		return
 	}
 
-	if page > 3 || page < 1 {
-		page = 3
-	}
+	// TODO: check for page not less than one or more than 3
 
-	ch := make(chan []models.Movie, page)
+	// TODO: Create a channel and read and merge data from channel by calling GoRoutine readJson
 
-	for i := 1; i <= page; i++ {
-		go readJson(i, ch)
-	}
-
-	var result []models.Movie
-	for i := 1; i <= page; i++ {
-		result = append(result, <-ch...)
-	}
+	// TODO: Append dat to result variable
 
 	service.ResponseHelper.RespondWithJSON(w, http.StatusOK, result)
 }
 
 func readJson(fileNumber int, ch chan []models.Movie) {
 	fileName := "json\\movies" + strconv.Itoa(fileNumber) + ".json"
-	jsonFile, err := os.Open(fileName)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println("Successfully Opened " + fileName)
-	defer jsonFile.Close()
-	byteValue, _ := ioutil.ReadAll(jsonFile)
-	var movies []models.Movie
-	json.Unmarshal(byteValue, &movies)
 
-	ch <- movies
+	// TODO: read data from file and then parse it then pass it back to channel
 }
